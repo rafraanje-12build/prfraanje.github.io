3accc2f95bee29e9ecc0310f0b8a0e9f94d854ef
Rufus Fraanje <p.r.fraanje@hhs.nl>
2020-12-20 16:17:21 +0100

    Nikola auto commit.
    Nikola version: 8.1.2

--- conf.py
+++ conf.py
@@ -1094,8 +1094,9 @@ PRETTY_URLS = True
 # Want to use KaTeX instead of MathJax? While KaTeX may not support every
 # feature yet, it's faster and the output looks better.
 # USE_KATEX = False
+USE_KATEX = True
 
-# KaTeX auto-render settings. If you want support for the $.$ syntax (which may
+# KaTeX a1uto-render settings. If you want support for the $.$ syntax (which may
 # conflict with running text!), just use this config:
 # KATEX_AUTO_RENDER = """
 # delimiters: [
